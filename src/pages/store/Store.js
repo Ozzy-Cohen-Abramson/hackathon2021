0b10e37595f82c115e0bd7c68fe3a001aa581ade
Yegor <yegorh8@gmail.com>
2021-09-01 10:27:43 +0300

    store init

--- src/pages/store/Store.js
+++ src/pages/store/Store.js
@@ -12,9 +12,33 @@ import AddBoxIcon from "@material-ui/icons/AddBox";
 import "./store.css";
 
 export default function Store() {
+
   return (
     <>
-      <div>this is the Store Page</div>
+      <div class="perspective">
+
+  {/* <label class="tab" for="tab-top">TOP</label>
+  <label class="tab" for="tab-front">FRONT</label>
+  <label class="tab" for="tab-bottom">BOTTOM</label>
+  <input type="radio" name="tabs" id="tab-top">
+  <input type="radio" name="tabs" id="tab-front">
+  <input type="radio" name="tabs" id="tab-bottom">  */}
+
+  <div class="cube">
+    <div class="tab-content">
+      <h1>TOP CONTENT</h1>
+      <p>THIS IS AWESOME</p>
+    </div>
+     <div class="tab-content">
+      <h1>FRONT CONTENT</h1>
+      <p>THIS IS COOL</p>
+    </div>
+    <div class="tab-content">
+      <h1>BOTTOM CONTENT</h1>
+      <p>THIS IS SWEET</p>
+    </div>
+  </div>
+</div>
     </>
   );
 }
